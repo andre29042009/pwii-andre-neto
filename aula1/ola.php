26d114e1b381625f99ac0101f966757579e2591e
Andre Garrido <andretrianaaa@gmail.com>
2025-02-25 16:32:05 -0300

    Aula de PHP com exemplos de concatenação de variáveis

---
+++ aula1/ola.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            echo "Ola Mundo!<br>";
+            $variavel = "Andre";
+            $numero = 4;
+            echo $variavel."<br>";
+            echo " <h1>${numero} ${variavel} <br> </h1>"";
+            $nome = "Andre";
+            $sobrenome = "Garrido";
+            echo $nome . " " . $sobrenome;
+        ?>
+    </body>
+</html>

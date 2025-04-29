01e209c96aa53e6c7dfc2b2274289395d995d5cc
Andre Garrido <andretrianaaa@gmail.com>
2025-04-29 16:24:00 -0300

    Aula de laços de repetição

---
+++ aula4/lacos.php
@@ -0,0 +1,28 @@
+<?php
+
+    // While
+
+    echo "While <br>";
+    $contador = 1;
+
+    while ($contador < 6) {
+        echo $contador, "<br>";
+        $contador++;
+    }
+
+    // Do while
+
+    echo "Do While <br>";
+    $contadorii = 1;
+
+    do {
+        echo $contadorii, "<br>";
+        $contadorii++;
+    } while ($contadorii < 6);
+
+    // For
+
+    for ($contadoriii = 0; $contadoriii <= 10; $xcontadoriii++) {
+        echo "The number is: $x <br>";
+    }
+?>

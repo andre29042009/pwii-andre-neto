9d1afec9623fa9f655f252861f3831595871cac8
Andre Garrido <andretrianaaa@gmail.com>
2025-05-06 16:27:13 -0300

    Aula 5

---
+++ aula5/index.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+
+        <form action="mensagem.php" method="POST">
+            <label for="msg">Digite sua mensagem:</label><br>
+            <input type=text name="msg" id="msg"><br>
+            <input type=text name="nome" id="nome"><br>
+            <input type="submit">
+        </form>
+
+    </body>
+</html>
